2e703a5e228035e68e72cdbf5cd087dc3f36a3db
Jeswin Santosh <jeswin.santosh@outlook.com>
2021-12-22 19:15:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
-from PIL import Image
+st.title("Iris Classifier- API")
 
-st.title("Iris Species Classifier")
-
-sl = st.slider('S e p a l - L e n g t h', 4.3, 7.9, 0.5)
-sw = st.slider('S e p a l - W i d t h', 2.0, 4.4, 0.5)
-pl = st.slider('P e t a l - L e n g t h', 1.0, 6.9, 0.5)
-pw = st.slider('P e t a l - W i d t h', 0.1, 2.5, 0.5)
+sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)
+sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)
+pl = st.slider('Petal Length', 1.0, 6.9, 0.5)
+pw = st.slider('Petal Width', 0.1,2.5, 0.5)
 
 from sklearn.datasets import load_iris
 iris = load_iris()
@@ -16,11 +14,5 @@ model = DecisionTreeClassifier()
 model.fit(iris.data,iris.target)
 
 op = model.predict([[sl,sw,pl,pw]])
-
-image1 = Image.open('Images/setosa.jpeg')
-image2 = Image.open('Images/versicolor.jpg')
-image3 = Image.open('Images/virginica.jpg')
-
-if op[0] == 0: st.image(image1)
-elif op[0] == 1: st.image(image2)
-else: st.image(image3)
+op = iris.target_names[op[0]]
+st.title(op)
